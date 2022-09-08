6093ed8e7f123877e236c603308dcdf5c25ac733
Duckenson jacquet <duckens244@hotmail.com>
2022-09-08 18:20:53 -0400

    Add some style

--- src/components/questions.jsx
+++ src/components/questions.jsx
@@ -1,5 +1,12 @@
 import React, { useEffect, useState } from "react";
 import url from "../urls";
+import RadioGroup from "@mui/material/RadioGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import FormControl from "@mui/material/FormControl";
+import FormLabel from "@mui/material/FormLabel";
+import Radio from "@mui/material/Radio";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
 
 export default function Question() {
   const urllink = url;
@@ -22,82 +29,162 @@ export default function Question() {
   };
 
   //Add Question
-  const handleSubmit = event => {
-    alert(libelle)
+  const handleSubmit = (event) => {
+    alert(libelle);
     event.preventDefault();
-    var data={
-        'libelle': libelle,
-        'ops1': ops1,
-        'ops2': ops2,
-        'ops3': ops3,
-        'ops4': ops4,
-        'ans': ans,
+    var data = {
+      libelle: libelle,
+      ops1: ops1,
+      ops2: ops2,
+      ops3: ops3,
+      ops4: ops4,
+      ans: ans,
     };
-    fetch('http://127.0.0.1:8000/api/add/',{
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            
-        },
-        body: JSON.stringify(data),
+    fetch("http://127.0.0.1:8000/api/add/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
     })
-    .then(res=>res.json())
-    .then(
-        (result) => {
-            alert(result['message'])
-            if (result['status']==='ok') {
-                window.location.reload();
+      .then((res) => res.json())
+      .then((result) => {
+        alert(result["message"]);
+        if (result["status"] === "ok") {
+          window.location.reload();
         }
-    }
-    )
-}
-const [libelle, setlibelle] = useState('')
-const [ops1, setops1] = useState('')
-const [ops2, setops2] = useState('')
-const [ops3, setops3] = useState('')
-const [ops4, setops4] = useState('')
-const [ans, setans] = useState('')
+      });
+  };
+  const [libelle, setlibelle] = useState("");
+  const [ops1, setops1] = useState("");
+  const [ops2, setops2] = useState("");
+  const [ops3, setops3] = useState("");
+  const [ops4, setops4] = useState("");
+  const [ans, setans] = useState("");
 
-//Update questions
+  //Update questions
 
   return (
-    <div>
-        <div>
-            <label>Add question</label>
-            <label>Libelle</label>
-            <input type="text" onChange={(e)=>setlibelle(e.target.value)} name="libelle" value={libelle}/><br/>
+    <div class="col-md-12 row d-flex justify-content-center">
+      <div className="col-md-6">
+        <label class="col-md-12 text-center">Ajouter question</label>
+        <label>Question</label>
+        <input
+          className="form-control"
+          type="text"
+          onChange={(e) => setlibelle(e.target.value)}
+          name="libelle"
+          value={libelle}
+        />
+        <div class="row d-flex col-md-12 justify-content-between">
+          <div class="col-md-6">
             <label>Option 1</label>
-            <input type="text" onChange={(e)=>setops1(e.target.value)} name="libelle" value={ops1}/><br/>
+            <input
+              type="text"
+              className="form-control"
+              onChange={(e) => setops1(e.target.value)}
+              name="libelle"
+              value={ops1}
+            />
+          </div>
+          <div class="col-md-6">
             <label>Option 2</label>
-            <input type="text" onChange={(e)=>setops2(e.target.value)} name="libelle" value={ops2}/><br/>
-            <label>Option 3</label>
-            <input type="text" onChange={(e)=>setops3(e.target.value)} name="libelle" value={ops3}/><br/>
-            <label>Option 4</label>
-            <input type="text" onChange={(e)=>setops4(e.target.value)} name="libelle" value={ops4}/><br/>
-            <label>Reponse </label>
-            <input type="text" onChange={(e)=>setans(e.target.value)} name="libelle" value={ans}/><br/>
-
-            <input type="submit" onClick={handleSubmit} namevalue="Valider"/>
+            <input
+              type="text"
+              className="form-control"
+              onChange={(e) => setops2(e.target.value)}
+              name="libelle"
+              value={ops2}
+            />
+          </div>
         </div>
-      <label>Liste of questions</label>
-      <table>
-        <thead>
-          <tr>
-            <th>Questions</th>
-          </tr>
-        </thead>
-        <tbody>
-          {questions.map((questions) => (
-            <tr key={questions.id}>
-              <td>
-                <label>{questions.libelle}</label><br/>
-                <input type="radio" name={questions.ops1}/>
-                <label>{questions.ops1}</label><br/>
-              </td>
+        <div class="row d-flex col-md-12 justify-content-between">
+          <div class="col-md-6">
+            {" "}
+            <label>Option 3</label>
+            <input
+              type="text"
+              className="form-control"
+              onChange={(e) => setops3(e.target.value)}
+              name="libelle"
+              value={ops3}
+            />
+          </div>
+          <div class="col-md-6">
+            <label>Option 4</label>
+            <input
+              type="text"
+              className="form-control"
+              onChange={(e) => setops4(e.target.value)}
+              name="libelle"
+              value={ops4}
+            />
+          </div>
+        </div>
+        <label>Reponse </label>
+        <input
+          type="text"
+          className="form-control"
+          onChange={(e) => setans(e.target.value)}
+          name="libelle"
+          value={ans}
+        />
+        <br />
+
+        <input
+          type="submit"
+          onClick={handleSubmit}
+          className="btn btn-primary mt-2"
+          value="Ajouter"
+        />
+      </div>
+      <div>
+        <label>Liste of questions</label>
+        <table>
+          <thead>
+            <tr>
+              <th>Questions</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {questions.map((questions) => (
+              <tr key={questions.id}>
+                <FormControl>
+                  <FormLabel id="demo-radio-buttons-group-label">
+                    {questions.libelle}
+                  </FormLabel>
+                  <RadioGroup
+                    aria-labelledby="demo-radio-buttons-group-label"
+                    defaultValue="female"
+                    name="radio-buttons-group"
+                  >
+                    <FormControlLabel
+                      value={questions.ops1}
+                      control={<Radio />}
+                      label={questions.ops1}
+                    />
+                    <FormControlLabel
+                      value={questions.ops2}
+                      control={<Radio />}
+                      label={questions.ops2}
+                    />
+                    <FormControlLabel
+                      value={questions.ops3}
+                      control={<Radio />}
+                      label={questions.ops3}
+                    />
+                    <FormControlLabel
+                      value={questions.ops4}
+                      control={<Radio />}
+                      label={questions.ops4}
+                    />
+                  </RadioGroup>
+                </FormControl>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
